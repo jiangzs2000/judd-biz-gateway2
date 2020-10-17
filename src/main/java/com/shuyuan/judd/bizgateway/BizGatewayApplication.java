b6848099314ceee8d293eb97ee5b4c65de36a5c8
member1 <jiangzs2003@163.com>
2020-10-17 14:48:12 +0800

    migrate from eureka to nacos

--- src/main/java/com/shuyuan/judd/bizgateway/BizGatewayApplication.java
+++ src/main/java/com/shuyuan/judd/bizgateway/BizGatewayApplication.java
@@ -3,10 +3,10 @@ package com.shuyuan.judd.bizgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class BizGatewayApplication {
